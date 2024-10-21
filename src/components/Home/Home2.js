66691ea5ce728d1399b5db7b8a6accc579bd72fa
T-SVETHA <154828084+T-SVETHA@users.noreply.github.com>
2024-10-21 11:04:18 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -46,7 +46,7 @@ function Home2() {
                   Modern Javascript Library and Frameworks
                 </b>
               </i>
-              like
+              ...;like
               <i>
                 <b className="purple"> React.js</b>
               </i>
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/C-Manoj"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
