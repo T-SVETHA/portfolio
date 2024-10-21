3ef246dc452de169e9e009dee5aa2f1d6793d1cb
T-SVETHA <154828084+T-SVETHA@users.noreply.github.com>
2024-10-21 12:53:13 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import chatify from "../../Assets/Projects/vmm.png";
+import vmm from "../../Assets/Projects/vmm.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
