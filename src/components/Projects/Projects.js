a9a2391707136bf3441b3ecd29ec864e32b933d2
T-SVETHA <154828084+T-SVETHA@users.noreply.github.com>
2024-10-21 12:32:39 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-
+import chatify from "../../Assets/Projects/vmm.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -17,7 +17,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              
+              imgPath={vmm}
               isBlog={false}
               title="Virtual Memory Management"
               description="Virtual memory management is a critical component of modern operating systems that enhances the efficiency and flexibility of memory usage. It abstracts physical memory, allowing systems to use disk space to simulate additional RAM. This mechanism enables multiple processes to run concurrently, providing each with a private address space while efficiently sharing physical memory resources.The project employs the Least Recently Used (LRU) page replacement algorithm. LRU is a widely-used strategy that tracks page usage over time and evicts the least recently accessed page when a page fault occurs. This approach is based on the assumption that pages used recently will likely be used again soon, thus optimizing memory utilization."
